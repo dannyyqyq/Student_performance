945d29b3bae37a71a770a8bf4b2aa0d8189e1c3c
Dannyyqt <dannyyqthong@gmail.com>
2025-01-18 00:29:59 +0800

    minor updates - git ignore/data_ingestion. no functionality changes

--- src/component/data_ingestion.py
+++ src/component/data_ingestion.py
@@ -10,6 +10,7 @@ from src.component.data_transformation import DataTransformation
 
 @dataclass
 class DataIngestionConfig:
+    # by default os.path.join == project root
     train_data_path: str = os.path.join("artifacts", "train.csv")
     test_data_path: str = os.path.join("artifacts", "test.csv")
     raw_data_path: str = os.path.join("artifacts", "data.csv")
